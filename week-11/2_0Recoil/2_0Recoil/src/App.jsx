957afae017563ad3278d9cade71a6b4b903dff32
BROCODES2024 <chetanreddyk394@gmail.com>
2025-01-14 17:18:30 +0530

    week11 Recoil

---
+++ week-11/2_0Recoil/2_0Recoil/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom } from "./store/atoms/counter";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  );
+}
+function Counter() {
+  return (
+    <div>
+      <Curcount />
+      <Increase />
+      <Decrease />
+    </div>
+  );
+}
+function Curcount() {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>;
+}
+function Increase() {
+  const setcount = useSetRecoilState(counterAtom);
+
+  function Increasecnt() {
+    setcount((c) => c + 1);
+  }
+  return (
+    <div>
+      <button onClick={Increasecnt}>Increase</button>
+    </div>
+  );
+}
+function Decrease() {
+  const setcount = useSetRecoilState(counterAtom);
+
+  function Decreasecnt() {
+    setcount((c) => c - 1);
+  }
+  return (
+    <div>
+      <button onClick={Decreasecnt}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
