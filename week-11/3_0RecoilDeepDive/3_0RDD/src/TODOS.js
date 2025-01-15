b7ad1a840fe739a5bd97219f0657ed9ae3616023
BROCODES2024 <chetanreddyk394@gmail.com>
2025-01-15 10:11:48 +0530

    week 11 done

---
+++ week-11/3_0RecoilDeepDive/3_0RDD/src/TODOS.js
@@ -0,0 +1,12 @@
+export const TODOS = [
+  {
+    id: 1,
+    title: "Go to Gym",
+    description: "Hit the gym",
+  },
+  {
+    id: 2,
+    title: "hello",
+    description: "hello world",
+  },
+];
