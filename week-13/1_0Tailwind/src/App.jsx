57e23f4c7f3cf9f9e0040a1ed7799f2afca8fa31
BROCODES2024 <chetanreddyk394@gmail.com>
2025-01-29 21:32:35 +0530

    Week 13

--- week-13/1_0Tailwind/src/App.jsx
+++ week-13/1_0Tailwind/src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/Buttons";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="bg-customBlue h-dvh flex flex-col space-y-4 p-4 justify-center">
-        <div>Webinar.gg</div>
-        <div>Verify Your Age</div>
-        <div>Please confirm your birth year.This data will not be stored.</div>
-        <input type="text" placeholder="Enter D.O.B" />
-        <button className="bg-buttoncolor">continue</button>
+      <div className="h-screen bg-blue-700">
+        <Button disabled={false}>Sign up</Button>
       </div>
     </>
   );
