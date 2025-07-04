7d2bcfbdc02c037c2999fc202cb29c4580ae9c7e
BROCODES2024 <chetanreddyk394@gmail.com>
2025-07-04 22:48:04 +0530

    promises revision

---
+++ week-2/3.js
@@ -0,0 +1,8 @@
+const map = new Map();
+map.set('name', 'John');
+map.set('age', 30);
+map.set('city', 'New York');
+console.log(map);
+console.log(map.get('name')); // John
+console.log(map.get('age')); // 30
+console.log(map.get('city')); // New York
